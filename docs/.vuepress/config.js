2ff4166e17225328e6f2392683fad994b337f07a
realfyz <76949080+realfyz@users.noreply.github.com>
2024-04-03 17:33:39 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '盐选推荐',
+    description: 'Be a Salt Reader...',
+    plugins: ['@vuepress/pwa'],
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }],
+      ],
+      themeConfig: {
+        logo: '/logo.png',
+      },
+  }
